8950e753ac00d8ee4ad6268534e90a3894576db3
Victor <victoradekunle312@gmail.com>
2022-04-23 02:38:18 +0000

    Creates the blogPost component

---
+++ src/Components/BlogNav/BlogNav.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './blogNav.css'
+
+const BlogNav = () => {
+  return (
+    <nav className='BlogNav'>
+        <li><a href="#">Latest</a></li>
+        <li><a href="#">Tech</a></li>
+        <li><a href="#">Business</a></li>
+        <li><a href="#">News</a></li>
+        <li><a href="#">Education</a></li>
+        <li><a href="#">Sports</a></li>
+    </nav>
+  )
+}
+
+export default BlogNav
--- src/Components/BlogPost/BlogPost.jsx
+++ src/Components/BlogPost/BlogPost.jsx
@@ -1,10 +1,55 @@
 import React from 'react'
 import './blogPost.css'
+import BlogNav from '../BlogNav/BlogNav'
+import BlogPostImg from '../../assets/images/blogPost.svg'
+import Field from '../../assets/images/field.jpg'
+import Anime from '../../assets/images/anime.jpg'
+import Forest from '../../assets/images/forest.jpg'
+import {AiOutlineArrowRight} from 'react-icons/ai';
 
 const BlogPost = () => {
   return (
     <section className="container blogs">
-        blog
+        <hr />
+        <BlogNav />
+        <div className="blogs-svg">
+          <img src={BlogPostImg} alt="blogsvg" />
+        </div>
+        <div className="blog">
+          <div className="blog-image">
+            <img src={Field} alt="pitch" />
+          </div>
+          <div className="blog-content">
+            <h2>FIFA World Cup</h2>
+            <h4>Victor Adekunle | April 23, 2022</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est harum blanditiis numquam ab magni nisi!</p>
+            <a href="#" className='btn'>View Post <AiOutlineArrowRight/></a>
+          </div>
+        </div>
+
+        <div className="blog">
+          <div className="blog-image">
+            <img src={Anime} alt="anime" />
+          </div>
+          <div className="blog-content">
+            <h2>Naruto vs Luffy vs Goku</h2>
+            <h4>Victor Adekunle | April 23, 2022</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est harum blanditiis numquam ab magni nisi!</p>
+            <a href="#" className='btn'>View Post <AiOutlineArrowRight/></a>
+          </div>
+        </div>
+
+        <div className="blog">
+          <div className="blog-image">
+            <img src={Forest} alt="forest" />
+          </div>
+          <div className="blog-content">
+            <h2>Clean Your Environment</h2>
+            <h4>Victor Adekunle | April 23, 2022</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est harum blanditiis numquam ab magni nisi!</p>
+            <a href="#" className='btn'>View Post <AiOutlineArrowRight/></a>
+          </div>
+        </div>
     </section>
   )
 }
