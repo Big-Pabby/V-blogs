d8f699bcc017a243a4f4f1a46b426ec52f873c1d
Big-Pabby <victoradekunle312@gmail.com>
2024-06-04 20:35:12 +0100

    settings

--- src/views/BlogDetail/BlogDetail.jsx
+++ src/views/BlogDetail/BlogDetail.jsx
@@ -1,6 +1,6 @@
-import React, {useState, useEffect} from 'react';
-import { useParams } from 'react-router-dom';
-import './blogDetail.css'
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import "./blogDetail.css";
 
 const BlogDetail = () => {
   const params = useParams();
@@ -8,26 +8,33 @@ const BlogDetail = () => {
   const [item, setItem] = useState({});
 
   useEffect(() => {
-    fetch(`https://secure-taiga-11377.herokuapp.com/blogPost/${params.id}`)
-      .then(Response => Response.json())
-      .then(post => {setItem(post)})
+    fetch(`https://taste-element-api-1.onrender.com/blogPost/${params.id}`)
+      .then((Response) => Response.json())
+      .then((post) => {
+        setItem(post);
+      });
   }, [params.id]);
 
   return (
     <div className="blog-detail">
-        <div className="container">
-            <div className="blog-Image">
-            <img src={item.blogimageurl} alt="blogimage" />
-            </div>
-            <div className="content">
-              <h2>{item.blogtitle}</h2>
-              <p>{item.blogby} | {item.created}</p>
-              <div className='category'>{item.blogcategory}</div>
-              <p className='topic' dangerouslySetInnerHTML={{__html: item.blogcontent}}></p>
-            </div>
+      <div className="container">
+        <div className="blog-Image">
+          <img src={item.blogimageurl} alt="blogimage" />
         </div>
+        <div className="content">
+          <h2>{item.blogtitle}</h2>
+          <p>
+            {item.blogby} | {item.created}
+          </p>
+          <div className="category">{item.blogcategory}</div>
+          <p
+            className="topic"
+            dangerouslySetInnerHTML={{ __html: item.blogcontent }}
+          ></p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default BlogDetail
+export default BlogDetail;
